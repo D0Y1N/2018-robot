e705d5ea2b213cc77c66d0d4d745ae1acce0858f
Caleb Smith <karagenit@outlook.com>
2018-04-17 09:21:21 -0400

    Update Left Side Scale Autons

--- src/com/techhounds/auton/paths/LeftScale.java
+++ src/com/techhounds/auton/paths/LeftScale.java
@@ -1,9 +1,11 @@
 package com.techhounds.auton.paths;
 
 import com.techhounds.auton.util.CollectCube;
+import com.techhounds.auton.util.CollectCubeRetryConditional;
 import com.techhounds.auton.util.DelayedCommand;
 import com.techhounds.auton.util.DriveArc;
 import com.techhounds.auton.util.DriveStraight;
+import com.techhounds.auton.util.DriveStraightRamp;
 import com.techhounds.auton.util.TurnToAngleGyro;
 import com.techhounds.intake.SetIntakePower;
 import com.techhounds.powerpack.SetElevatorPosition;
@@ -20,45 +22,58 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class LeftScale extends CommandGroup {
 
     public LeftScale() {
+    	// TODO timeouts
+    	    	
        	// Set tilt/elevator
     	addParallel(new SetTiltPosition(TiltPosition.DOWN));    	
     	addParallel(new DelayedCommand(new SetElevatorPosition(ElevatorPosition.SCALE), 1.5));
 
     	// drive up & curve
-    	addSequential(new DriveStraight(210, 0.75), 6);
-    	addSequential(new DriveStraight(30, 0.5), 1);
-    	addSequential(new DriveArc(25, 50, 0.25, 0.45), 2); // curve right
+    	addSequential(new DriveStraightRamp(80, 0.2, 1));
+    	addSequential(new DriveArc(40, 50, 0.9, 1));
+    	addSequential(new DriveStraight(40, 1));
+    	addSequential(new DriveStraightRamp(100, 1, 0));
     	
     	// eject the cube
     	addParallel(new SetTiltPosition(TiltPosition.MIDDLE));
-    	addSequential(new SetIntakePower(-0.4), 1);
+    	addSequential(new SetIntakePower(-0.4), 0.5);
     	
     	// back off and reset
     	addParallel(new SetTiltPosition(Tilt.POS_DOWN));
-    	addSequential(new DriveStraight(-15, -0.4), 2);
-    	addSequential(new TurnToAngleGyro(132), 2);
+    	addSequential(new TurnToAngleGyro(160), 2);
     	addParallel(new SetElevatorPosition(ElevatorPosition.COLLECT));
     	
     	// grab second cube
     	addSequential(new DriveArc(50, 60, 0.4, 0.5), 2);
-    	addSequential(new CollectCube(25, 175), 3);
+    	addSequential(new CollectCube(25), 3);
     	
-    	// retry grab if we didn't get it
-//    	addSequential(new RetryCollectCube(40), 3);
-//    	addSequential(new RetryCollectCube(40), 3);
+		// retry collection
+		addSequential(new CollectCubeRetryConditional());
+		addSequential(new CollectCubeRetryConditional());
+		addSequential(new CollectCubeRetryConditional());
+
+    	// drive back to scale
+    	addSequential(new TurnToAngleGyro(135), 1);
+    	addParallel(new SetElevatorPosition(ElevatorPosition.SCALE));
+    	addSequential(new DriveStraight(-60, -0.4), 3);
+    	addSequential(new TurnToAngleGyro(30), 1);
     	
     	// place in scale
-    	addSequential(new TurnToAngleGyro(135), 1);
-    	addParallel(new DelayedCommand(new SetElevatorPosition(ElevatorPosition.SCALE), 1));
     	addParallel(new SetTiltPosition(Tilt.POS_MID));
-    	addSequential(new DriveStraight(-45, -0.4), 3);
-    	addSequential(new TurnToAngleGyro(60), 1);
-    	addSequential(new DriveStraight(20, 0.3), 2);
-    	addSequential(new SetIntakePower(-0.5), 1);
+    	addSequential(new SetIntakePower(-0.5), 0.5);
     	
-    	// back off
-    	addSequential(new DriveStraight(-24, -0.3), 2);
+    	// back off and reset
     	addParallel(new SetTiltPosition(Tilt.POS_DOWN));
-    	addSequential(new SetElevatorPosition(ElevatorPosition.COLLECT), 2);
+    	addSequential(new TurnToAngleGyro(140), 2);
+    	addParallel(new SetElevatorPosition(ElevatorPosition.COLLECT));
+    	
+    	// grab third cube
+    	addSequential(new DriveArc(80, 60, 0.4, 0.6), 2);
+    	addSequential(new CollectCube(25), 3);
+    	
+		// retry collection
+		addSequential(new CollectCubeRetryConditional());
+		addSequential(new CollectCubeRetryConditional());
+		addSequential(new CollectCubeRetryConditional());
     }
 }
--- src/com/techhounds/auton/paths/RightScaleCross.java
+++ src/com/techhounds/auton/paths/RightScaleCross.java
@@ -1,7 +1,11 @@
 package com.techhounds.auton.paths;
 
-import com.techhounds.auton.util.DelayedCommand;
+import com.techhounds.auton.util.CollectCube;
+import com.techhounds.auton.util.CollectCubeRetryConditional;
+import com.techhounds.auton.util.DriveAngle;
+import com.techhounds.auton.util.DriveArc;
 import com.techhounds.auton.util.DriveStraight;
+import com.techhounds.auton.util.DriveStraightRamp;
 import com.techhounds.auton.util.TurnToAngleGyro;
 import com.techhounds.intake.SetIntakePower;
 import com.techhounds.powerpack.SetElevatorPosition;
@@ -17,27 +21,54 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class RightScaleCross extends CommandGroup {
 
     public RightScaleCross() {
+    	// TODO timeouts
+    	
     	addParallel(new SetTiltPosition(Tilt.POS_DOWN));
     	
     	// drive across
-    	addSequential(new DriveStraight(200, 0.7), 8);
-    	addSequential(new DriveStraight(18, 0.4), 2);
-    	addSequential(new TurnToAngleGyro(85), 3);
-    	addParallel(new DelayedCommand(new SetElevatorPosition(ElevatorPosition.SCALE), 4));
-    	addParallel(new DelayedCommand(new SetTiltPosition(Tilt.POS_MID), 4));
-    	addSequential(new DriveStraight(184, 0.6), 8);
+    	addSequential(new DriveStraightRamp(100, 0.2, 1));
+    	addSequential(new DriveStraightRamp(100, 1, 0.5));
+    	addSequential(new DriveAngle(85, 0.4, 0.6));
+    	
+    	addSequential(new DriveStraightRamp(45, 0.5, 0.75));
+    	addParallel(new SetTiltPosition(Tilt.POS_MID));
+    	addSequential(new DriveStraightRamp(45, 0.75, 0.5));
+    	addSequential(new DriveStraightRamp(30, 0.5, 0.65));
+    	addParallel(new SetElevatorPosition(ElevatorPosition.SCALE));
+    	addSequential(new DriveStraightRamp(65, 0.65, 0));
     	
     	// put in scale
-    	addSequential(new DriveStraight(30, 0.4), 2);
     	addSequential(new TurnToAngleGyro(-30), 2);
     	addSequential(new DriveStraight(12, 0.4), 2);
-    	addSequential(new SetIntakePower(-0.5), 1);
+    	addSequential(new SetIntakePower(-0.5), 0.5);
     	
-    	// back off
-    	addParallel(new DelayedCommand(new SetElevatorPosition(ElevatorPosition.COLLECT), 1));
-    	addSequential(new DriveStraight(-20, -0.4), 2);
-    	
-    	// align to second cube
+    	// back off and reset
+    	addParallel(new SetTiltPosition(Tilt.POS_DOWN));
     	addSequential(new TurnToAngleGyro(-160), 2);
+    	addParallel(new SetElevatorPosition(ElevatorPosition.COLLECT));
+    	
+    	// grab second cube
+    	addSequential(new DriveArc(60, 50, 0.5, 0.4), 2);
+    	addSequential(new CollectCube(25), 3);
+    	
+		// retry collection
+		addSequential(new CollectCubeRetryConditional());
+		addSequential(new CollectCubeRetryConditional());
+		addSequential(new CollectCubeRetryConditional());
+		
+		// drive back to scale
+    	addSequential(new TurnToAngleGyro(-135), 1);
+    	addParallel(new SetElevatorPosition(ElevatorPosition.SCALE));
+    	addSequential(new DriveStraight(-60, -0.4), 3);
+    	addSequential(new TurnToAngleGyro(-30), 1);
+    	
+    	// place in scale
+    	addParallel(new SetTiltPosition(Tilt.POS_MID));
+    	addSequential(new SetIntakePower(-0.5), 0.5);
+    	
+    	// back off and reset
+    	addParallel(new SetTiltPosition(Tilt.POS_DOWN));
+    	addSequential(new TurnToAngleGyro(-140), 2);
+    	addParallel(new SetElevatorPosition(ElevatorPosition.COLLECT));
     }
 }
