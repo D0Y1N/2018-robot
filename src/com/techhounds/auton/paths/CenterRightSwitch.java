d50be413eefbcb8a97503d6e7cd25bb67618612d
Caleb Smith <karagenit@outlook.com>
2018-04-20 16:46:03 -0400

    Tune Center Auton

--- src/com/techhounds/auton/paths/CenterRightSwitch.java
+++ src/com/techhounds/auton/paths/CenterRightSwitch.java
@@ -3,7 +3,9 @@ package com.techhounds.auton.paths;
 import com.techhounds.arm.GrabCube;
 import com.techhounds.auton.util.CollectCube;
 import com.techhounds.auton.util.DelayedCommand;
+import com.techhounds.auton.util.DriveAngle;
 import com.techhounds.auton.util.DriveStraight;
+import com.techhounds.auton.util.DriveStraightUntilDetected;
 import com.techhounds.auton.util.TurnToAngleGyro;
 import com.techhounds.intake.IntakeUntilDetected;
 import com.techhounds.intake.SetIntakePower;
@@ -25,34 +27,36 @@ public class CenterRightSwitch extends CommandGroup {
     	addParallel(new DelayedCommand(new SetTiltPosition(Tilt.POS_DOWN), 1));
     	addParallel(new DelayedCommand(new SetElevatorPosition(ElevatorPosition.SWITCH), 1.5));
     	
-    	// drive in s pattern
-    	addSequential(new DriveStraight(15, 0.4), 1);
-    	addSequential(new TurnToAngleGyro(25), 0.75);
-    	addSequential(new DriveStraight(60, 0.6), 3);
-    	addSequential(new DriveStraight(20, 0.4), 1);
+    	// drive up to switch
+    	addSequential(new DriveAngle(30, 0, 0.4), 1);
+    	addSequential(new DriveStraight(65, 0.6), 3);
+    	addSequential(new DriveStraight(30, 0.4), 1);
+    	addSequential(new TurnToAngleGyro(10), 0.5);
     	
     	// eject cube
-    	addSequential(new TurnToAngleGyro(0), 1);
     	addSequential(new SetIntakePower(-0.5), 0.5);
     	
+    	// LAST CHECK
+    	
     	// back up to starting position
-    	addSequential(new DriveStraight(-10, -0.4), 1);
-    	addSequential(new TurnToAngleGyro(35), 1);
+//    	addSequential(new DriveStraight(-10, -0.4), 1);
+//    	addSequential(new TurnToAngleGyro(-30), 1);
+    	addSequential(new DriveAngle(37, -0.4, 0), 1);
     	addParallel(new DelayedCommand(new SetElevatorPosition(ElevatorPosition.COLLECT), 0.5));
     	addSequential(new DriveStraight(-55, -0.6), 3);
     	addSequential(new DriveStraight(-20, -0.4), 2);
     	
-    	// grab another one
+//    	// grab another one
     	addSequential(new TurnToAngleGyro(0), 1.5);
     	addParallel(new GrabCube(), 3);
     	addParallel(new IntakeUntilDetected(), 3);
-    	addSequential(new DriveStraight(40, 0.5), 1);
-    	addSequential(new DriveStraight(10, 0.3), 0.5);
+    	addSequential(new DriveStraightUntilDetected(40, 0.5), 1);
+    	addSequential(new DriveStraightUntilDetected(10, 0.3), 0.5);
     	
     	// line up to switch again
-    	addSequential(new WaitCommand(0.5));
-    	addSequential(new TurnToAngleGyro(70), 1.5);
-    	addSequential(new DriveStraight(45, 0.5), 2);
+//    	addSequential(new WaitCommand(0.5));
+    	addSequential(new TurnToAngleGyro(70), 1);
+    	addSequential(new DriveStraight(40, 0.5), 2);
     	addParallel(new SetElevatorPosition(ElevatorPosition.SWITCH));
     	addSequential(new TurnToAngleGyro(0), 1);
     	addSequential(new DriveStraight(12, 0.4), 1);
@@ -60,8 +64,8 @@ public class CenterRightSwitch extends CommandGroup {
     	addSequential(new SetIntakePower(-0.5), 0.5);
     	
     	addParallel(new DelayedCommand(new SetElevatorPosition(ElevatorPosition.COLLECT), 0.5));
-    	addSequential(new DriveStraight(-22, -0.4), 1);
-    	addSequential(new TurnToAngleGyro(-62), 1);
+    	addSequential(new DriveStraight(-26, -0.4), 1);
+    	addSequential(new TurnToAngleGyro(-55), 1);
     	addSequential(new CollectCube(35), 2);
     }
 }
