0f8b0280e838f482949197a03885d17b6c804a7a
Caleb Smith <karagenit@outlook.com>
2018-04-21 11:22:41 -0400

    Tune Cross Auton

--- src/com/techhounds/auton/paths/LeftScaleCross.java
+++ src/com/techhounds/auton/paths/LeftScaleCross.java
@@ -1,5 +1,6 @@
 package com.techhounds.auton.paths;
 
+import com.techhounds.auton.util.CollectCube;
 import com.techhounds.auton.util.DelayedCommand;
 import com.techhounds.auton.util.DriveAngle;
 import com.techhounds.auton.util.DriveStraight;
@@ -9,6 +10,7 @@ import com.techhounds.intake.SetIntakePower;
 import com.techhounds.powerpack.SetElevatorPosition;
 import com.techhounds.powerpack.SetElevatorPosition.ElevatorPosition;
 import com.techhounds.tilt.SetTiltPosition;
+import com.techhounds.tilt.SetTiltPosition.TiltPosition;
 import com.techhounds.tilt.Tilt;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -28,22 +30,23 @@ public class LeftScaleCross extends CommandGroup {
     	addSequential(new DriveStraight(100, 1, 0));
     	addSequential(new DriveStraightRamp(30, 1, 0.5, 0));
 //    	addSequential(new TurnToAngleGyro(-85), 3);
-    	addSequential(new DriveAngle(-65, 0.6, 0.2));
-    	addParallel(new DelayedCommand(new SetTiltPosition(Tilt.POS_MID), 4));
+    	addSequential(new DriveAngle(-75, 0.6, 0.2));
     	addSequential(new DriveStraight(195, 0.6, -90));
+    	addParallel(new DelayedCommand(new SetTiltPosition(Tilt.POS_MID), 1));
+
     	
     	// put in scale
     	addParallel(new SetElevatorPosition(ElevatorPosition.SCALE));
     	addSequential(new TurnToAngleGyro(20, 2, 0.3));
     	addSequential(new DriveStraight(40, 0.4), 2);
     	addSequential(new WaitCommand(0.5));
-    	addSequential(new SetIntakePower(-0.35), 1);
-//    	
-//    	// back off
-//    	addParallel(new DelayedCommand(new SetElevatorPosition(ElevatorPosition.COLLECT), 1));
-//    	addSequential(new DriveStraight(-20, -0.4), 2);
-//    	
-//    	// align to second cube
-//    	addSequential(new TurnToAngleGyro(160), 2);
+    	addSequential(new SetIntakePower(-0.35), 0.5);
+    	
+    	// grab second cube
+    	addParallel(new SetTiltPosition(TiltPosition.DOWN));
+    	addSequential(new TurnToAngleGyro(166.5, 2, 0.3));
+    	addParallel(new SetElevatorPosition(ElevatorPosition.COLLECT));
+    	addSequential(new DriveStraight(20, 0.3), 3);
+    	addSequential(new CollectCube(25));
     }
 }
