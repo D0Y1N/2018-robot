4e33f8730c18575e93f2f0cf98e95c8036ed800c
Caleb Smith <karagenit@outlook.com>
2018-04-21 11:00:10 -0400

    Tune Cross Auton

--- src/com/techhounds/auton/paths/LeftScaleCross.java
+++ src/com/techhounds/auton/paths/LeftScaleCross.java
@@ -1,16 +1,14 @@
 package com.techhounds.auton.paths;
 
 import com.techhounds.auton.util.DelayedCommand;
+import com.techhounds.auton.util.DriveAngle;
 import com.techhounds.auton.util.DriveStraight;
+import com.techhounds.auton.util.DriveStraightRamp;
 import com.techhounds.auton.util.TurnToAngleGyro;
-import com.techhounds.intake.SetIntakePower;
-import com.techhounds.powerpack.SetElevatorPosition;
-import com.techhounds.powerpack.SetElevatorPosition.ElevatorPosition;
 import com.techhounds.tilt.SetTiltPosition;
 import com.techhounds.tilt.Tilt;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import edu.wpi.first.wpilibj.command.WaitCommand;
 
 /**
  *
@@ -18,27 +16,30 @@ import edu.wpi.first.wpilibj.command.WaitCommand;
 public class LeftScaleCross extends CommandGroup {
 
     public LeftScaleCross() {
+    	// TODO timeouts
     	addParallel(new SetTiltPosition(Tilt.POS_DOWN));
     	
     	// drive across
-    	addSequential(new DriveStraight(200, 0.7), 8);
-    	addSequential(new DriveStraight(18, 0.4), 2);
-    	addSequential(new TurnToAngleGyro(-85), 3);
-    	addParallel(new DelayedCommand(new SetElevatorPosition(ElevatorPosition.SCALE), 4));
+    	addSequential(new DriveStraightRamp(40, 0.3, 1, 0));
+    	addSequential(new DriveStraight(100, 1, 0));
+    	addSequential(new DriveStraightRamp(30, 1, 0.5, 0));
+//    	addSequential(new TurnToAngleGyro(-85), 3);
+    	addSequential(new DriveAngle(-75, 0.6, 0.2));
+//    	addParallel(new DelayedCommand(new SetElevatorPosition(ElevatorPosition.SCALE), 4));
     	addParallel(new DelayedCommand(new SetTiltPosition(Tilt.POS_MID), 4));
-    	addSequential(new DriveStraight(200, 0.6), 8);
+    	addSequential(new DriveStraight(185, 0.6, -90));
     	
     	// put in scale
-    	addSequential(new TurnToAngleGyro(10), 2);
-    	addSequential(new DriveStraight(12, 0.4), 2);
-    	addSequential(new WaitCommand(0.5));
-    	addSequential(new SetIntakePower(-0.35), 1);
-    	
-    	// back off
-    	addParallel(new DelayedCommand(new SetElevatorPosition(ElevatorPosition.COLLECT), 1));
-    	addSequential(new DriveStraight(-20, -0.4), 2);
-    	
-    	// align to second cube
-    	addSequential(new TurnToAngleGyro(160), 2);
+    	addSequential(new TurnToAngleGyro(15), 2);
+//    	addSequential(new DriveStraight(12, 0.4), 2);
+//    	addSequential(new WaitCommand(0.5));
+//    	addSequential(new SetIntakePower(-0.35), 1);
+//    	
+//    	// back off
+//    	addParallel(new DelayedCommand(new SetElevatorPosition(ElevatorPosition.COLLECT), 1));
+//    	addSequential(new DriveStraight(-20, -0.4), 2);
+//    	
+//    	// align to second cube
+//    	addSequential(new TurnToAngleGyro(160), 2);
     }
 }
