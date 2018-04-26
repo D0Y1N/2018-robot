3f5313f4d9a884a9bacb190714c0bc1de166abed
Caleb Smith <karagenit@outlook.com>
2018-04-26 11:07:11 -0400

    Speed Up Cross Auton

--- src/com/techhounds/auton/paths/LeftScaleCross.java
+++ src/com/techhounds/auton/paths/LeftScaleCross.java
@@ -25,13 +25,12 @@ public class LeftScaleCross extends CommandGroup {
     	addParallel(new SetTiltPosition(Tilt.POS_DOWN));
     	
     	// drive across
-    	addSequential(new DriveStraightRamp(40, 0.3, 1, 0));
+    	addSequential(new DriveStraightRamp(40, 0.4, 1, 0));
     	addSequential(new DriveStraight(100, 1, 0));
     	addSequential(new DriveStraightRamp(30, 1, 0.5, 0));
 //    	addSequential(new TurnToAngleGyro(-85), 3);
     	addSequential(new DriveAngle(-75, 0.6, 0.2));
     	addParallel(new DelayedCommand(new SetTiltPosition(Tilt.POS_MID), 0.5));
-//    	addSequential(new DriveStraight(195, 0.6, -90)); // TODO ramping speedup
     	
     	addSequential(new DriveStraightRamp(45, 0.6, 1, -90));
     	addSequential(new DriveStraightRamp(45, 1, 0.5, -90));
@@ -40,22 +39,21 @@ public class LeftScaleCross extends CommandGroup {
     	
     	// put in scale
     	addParallel(new SetElevatorPosition(ElevatorPosition.SCALE));
-    	addSequential(new TurnToAngleGyro(20, 1.5, 0.3));
+    	addSequential(new TurnToAngleGyro(20), 1.5);
     	addSequential(new DriveStraight(40, 0.4, 20), 2);
-//    	addSequential(new WaitCommand(0.5)); // TODO remove
     	addSequential(new SetIntakePower(-0.35), 0.5);
     	
     	// grab second cube
     	addParallel(new SetTiltPosition(TiltPosition.DOWN));
-    	addSequential(new TurnToAngleGyro(166.5, 1.5, 0.3));
+    	addSequential(new TurnToAngleGyro(166.5), 1.5);
     	addParallel(new SetElevatorPosition(ElevatorPosition.COLLECT));
-    	addSequential(new DriveStraight(20, 0.3, 166.5), 3); // TODO speedup
-    	addSequential(new CollectCube(25));
+    	addSequential(new DriveStraight(20, 0.45, 166.5), 3);
+    	addSequential(new CollectCube(35));
     	
     	// place second
     	addSequential(new DriveStraight(-40, -0.4));
     	addParallel(new SetElevatorPosition(ElevatorPosition.SCALE));
-    	addSequential(new TurnToAngleGyro(20, 1, 0.3));
+    	addSequential(new TurnToAngleGyro(20), 1.5);
     	addSequential(new SetIntakePower(-0.35), 0.5);
     }
 }
